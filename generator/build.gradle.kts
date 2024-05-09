d1af2d6d2f038e81ad704b113fa9a1772b6f1f28
WuYa <crowforkotlin@gmail.com>
2024-05-09 16:23:13 +0800

    :fire:INIT

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,1 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins { kotlin("jvm") }
+
+dependencies {
+    implementation(libs.kotlinpoet)
+    implementation(libs.kotlinpoet.ksp)
+    implementation(libs.ksp.api) { exclude(module = "kotlin-reflect") }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+            "-Xopt-in=com.squareup.kotlinpoet.ksp.KotlinPoetKspPreview",
+            "-Xopt-in=com.google.devtools.ksp.KspExperimental",
+            "-Xopt-in=kotlin.contracts.ExperimentalContracts"
+        )
+    }
+}
